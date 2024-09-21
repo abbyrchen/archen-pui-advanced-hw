ac2fdac36acd40ae6011c5ff0d3ca51aae8e4a74
Abby Chen <archen@andrew.cmu.edu>
2024-09-21 16:05:40 -0400

    translated html to jsx

---
+++ solution-hw3/src/views/home/index.js
@@ -0,0 +1,18 @@
+// create react component for homepage
+
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <header>
+                <h1>Simple Homepage</h1>
+            </header>
+            <main>
+                <p>welcome!</p>
+            </main>
+        </div>
+    );
+}
+
+export default Homepage;
