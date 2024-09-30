5a185a85f4aac5778bf287cdc8306763960b55b5
Abby Chen <archen@andrew.cmu.edu>
2024-09-30 00:45:24 -0400

    created solution-hw4

---
+++ solution-hw4/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './views/home/index.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
